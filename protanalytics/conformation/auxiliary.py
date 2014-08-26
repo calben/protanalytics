3695aad339b8909264bd04699927bcd0e398379b
Calem Bendell <calem.j.bendell@gmail.com>
2014-08-26 00:17:42 -0700

    refactoring to suggest anglecossin general nature

--- protanalytics/conformation/auxiliary.py
+++ protanalytics/conformation/auxiliary.py
@@ -15,13 +15,13 @@ def angle_distance_metric(a, b):
   return 360 - d if d > 180 else d
   
 
-def z_angle_matrix_to_z_sin_cos_matrix(z_angle_matrix):
+def angle_matrix_to_sin_cos_matrix(angle_matrix):
   z_sin_matrix = z_angle_matrix.apply(np.sin)
   z_sin_matrix.columns = map(lambda x : x + "--SIN", z_angle_matrix.columns)
   z_cos_matrix = z_angle_matrix.apply(np.cos)
   z_cos_matrix.columns = map(lambda x : x + "--COS", z_angle_matrix.columns)
   return pd.concat([z_sin_matrix, z_cos_matrix], axis=1, join='outer', join_axes=None, ignore_index=False, keys=None, levels=None, names=None, verify_integrity=True)
 
-def z_sin_cos_matrix_to_z_angle_matrix(z_sin_cos_matrix):
+def sin_cos_matrix_to_angle_matrix(sin_cos_matrix):
   z_sin_matrix = z_sin_cos_matrix[0:(len(z_sin_cos_matrix)/2)]
   z_cos_matrix = z_sin_cos_matrix[(len(z_sin_cos_matrix)/2):0]
